9702b9c6697fb97e30ee3e63ec07c3aacff94801
koipkoi <hrh6914@gmail.com>
2021-03-17 16:52:06 +0900

    Update SaveInstanceStateFieldHelper.java
    프래그먼트에 적용되지 않는 현상 수정

---
+++ app/src/main/java/com/nextstory/sample/ui/fragment/TestFragment.java
@@ -0,0 +1,50 @@
+package com.nextstory.sample.ui.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.nextstory.di.Inject;
+import com.nextstory.field.NonNullLiveData;
+import com.nextstory.fragment.BaseFragment;
+import com.nextstory.sample.databinding.FragmentTestBinding;
+import com.nextstory.sample.ui.dialog.Test2Dialog;
+import com.nextstory.sample.ui.dialog.TestDialog;
+
+/**
+ * @author troy
+ * @version 1.0
+ * @since 1.0
+ */
+public final class TestFragment extends BaseFragment<FragmentTestBinding> {
+    public final NonNullLiveData<String> safeValue = new NonNullLiveData<>("first", true);
+    public final NonNullLiveData<String> unsafeValue = new NonNullLiveData<>("first");
+
+    @Inject
+    TestDialog testDialog;
+    @Inject
+    Test2Dialog test2Dialog;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getBinding().setFragment(this);
+        getBinding().setLifecycleOwner(this);
+    }
+
+    public void onInjectTestClick() {
+        testDialog.show();
+    }
+
+    public void onInjectTest2Click() {
+        test2Dialog.show();
+    }
+
+    public void onSafeFieldTestClick() {
+        safeValue.setValue("second");
+        unsafeValue.setValue("second");
+    }
+}
--- library/src/main/java/com/nextstory/field/SaveInstanceStateFieldHelper.java
+++ library/src/main/java/com/nextstory/field/SaveInstanceStateFieldHelper.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.startup.Initializer;
 
 import com.nextstory.util.SimpleActivityLifecycleCallbacks;
@@ -19,16 +23,42 @@ import java.util.List;
 
 /**
  * 필드 상태 저장 구현
- * 액티비티 내 {@link SaveInstanceStateField}를 구현한 필드들의 상태를 저장 및 복구함.
+ * 액티비티, 프래그먼트 내 {@link SaveInstanceStateField}를 구현한 필드들의 상태를 저장 및 복구함.
  *
  * @author troy
- * @version 1.0
+ * @version 1.0.1
  * @since 1.0
  */
 @SuppressWarnings("UnusedDeclaration")
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 public final class SaveInstanceStateFieldHelper
         implements Initializer<Object>, SimpleActivityLifecycleCallbacks {
+    private final FragmentManager.FragmentLifecycleCallbacks fragmentLifecycleCallbacks
+            = new FragmentManager.FragmentLifecycleCallbacks() {
+        @Override
+        public void onFragmentViewCreated(@NonNull FragmentManager fragmentManager,
+                                          @NonNull Fragment fragment,
+                                          @NonNull View v,
+                                          @Nullable Bundle savedInstanceState) {
+            if (savedInstanceState != null) {
+                List<SaveInstanceStateField> fields = findFields(fragment);
+                for (SaveInstanceStateField field : fields) {
+                    field.onRestoreInstanceState(savedInstanceState);
+                }
+            }
+        }
+
+        @Override
+        public void onFragmentSaveInstanceState(@NonNull FragmentManager fragmentManager,
+                                                @NonNull Fragment fragment,
+                                                @NonNull Bundle outState) {
+            List<SaveInstanceStateField> fields = findFields(fragment);
+            for (SaveInstanceStateField field : fields) {
+                field.onSaveInstanceState(outState);
+            }
+        }
+    };
+
     @NonNull
     @Override
     public Object create(@NonNull Context context) {
@@ -52,6 +82,20 @@ public final class SaveInstanceStateFieldHelper
                 field.onRestoreInstanceState(savedInstanceState);
             }
         }
+        if (activity instanceof FragmentActivity) {
+            FragmentActivity fragmentActivity = (FragmentActivity) activity;
+            fragmentActivity.getSupportFragmentManager()
+                    .registerFragmentLifecycleCallbacks(fragmentLifecycleCallbacks, true);
+        }
+    }
+
+    @Override
+    public void onActivityDestroyed(@NonNull Activity activity) {
+        if (activity instanceof FragmentActivity) {
+            FragmentActivity fragmentActivity = (FragmentActivity) activity;
+            fragmentActivity.getSupportFragmentManager()
+                    .unregisterFragmentLifecycleCallbacks(fragmentLifecycleCallbacks);
+        }
     }
 
     @Override
@@ -66,16 +110,16 @@ public final class SaveInstanceStateFieldHelper
     /**
      * 필드 내 {@link SaveInstanceStateField} 구현 목록를 반환.
      *
-     * @param activity 액티비티
+     * @param instance 인스턴스
      * @return 목록
      */
-    private List<SaveInstanceStateField> findFields(Activity activity) {
+    private List<SaveInstanceStateField> findFields(Object instance) {
         List<SaveInstanceStateField> result = new ArrayList<>();
-        Field[] fields = activity.getClass().getDeclaredFields();
+        Field[] fields = instance.getClass().getDeclaredFields();
         for (Field field : fields) {
             boolean isAccessible = field.isAccessible();
             try {
-                Object object = field.get(activity);
+                Object object = field.get(instance);
                 if (object instanceof SaveInstanceStateField) {
                     SaveInstanceStateField saveInstanceStateField = (SaveInstanceStateField) object;
                     saveInstanceStateField.setKey(field.getName());
