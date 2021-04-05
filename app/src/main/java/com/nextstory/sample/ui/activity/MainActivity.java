845785b02089ce969a741a1918e08db41b5cf0a9
koipkoi <hrh6914@gmail.com>
2021-04-05 13:54:54 +0900

    로딩 레이아웃 추가

--- app/src/main/java/com/nextstory/sample/ui/activity/MainActivity.java
+++ app/src/main/java/com/nextstory/sample/ui/activity/MainActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 
 import com.nextstory.activity.BaseActivity;
 import com.nextstory.field.NonNullLiveData;
+import com.nextstory.field.OnDestroyDisposables;
 import com.nextstory.field.SafeData;
 import com.nextstory.sample.databinding.ActivityMainBinding;
 import com.nextstory.sample.ui.dialog.Test2Dialog;
 import com.nextstory.sample.ui.dialog.TestDialog;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 /**
  * @author troy
@@ -19,8 +24,11 @@ import dagger.hilt.android.AndroidEntryPoint;
  */
 @AndroidEntryPoint
 public final class MainActivity extends BaseActivity<ActivityMainBinding> {
+    public final NonNullLiveData<Boolean> progress = new NonNullLiveData<>(false);
     public final NonNullLiveData<String> safeValue = new NonNullLiveData<>("first", true);
     public final NonNullLiveData<String> unsafeValue = new NonNullLiveData<>("first");
+
+    private final OnDestroyDisposables disposables = new OnDestroyDisposables(this);
     private final SafeData<Boolean> localeToggle = new SafeData<>(false);
 
     @Override
@@ -32,6 +40,12 @@ public final class MainActivity extends BaseActivity<ActivityMainBinding> {
 
         getBinding().setActivity(this);
         getBinding().setLifecycleOwner(this);
+
+        progress.setValue(true);
+        disposables.add(Completable.timer(3000L, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(() -> progress.setValue(false)));
     }
 
     public void onDialogTestClick() {
---
+++ library/src/main/java/com/nextstory/widget/LoadingLayout.java
@@ -0,0 +1,113 @@
+package com.nextstory.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.BindingAdapter;
+
+import com.nextstory.R;
+
+/**
+ * 로딩 레이아웃
+ *
+ * @author troy
+ * @since 1.1
+ */
+@SuppressWarnings("UnusedDeclaration")
+public final class LoadingLayout extends FrameLayout {
+    private final int loadingViewId;
+    private boolean isLoading;
+    private View loadingView = null;
+
+    public LoadingLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public LoadingLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LoadingLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        if (attrs == null) {
+            loadingViewId = NO_ID;
+            isLoading = false;
+        } else {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.LoadingLayout, 0, 0);
+            loadingViewId = a.getResourceId(
+                    R.styleable.LoadingLayout_loadingViewId,
+                    NO_ID);
+            isLoading = a.getBoolean(
+                    R.styleable.LoadingLayout_loading,
+                    false);
+            a.recycle();
+        }
+
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        requestFocus();
+    }
+
+    /**
+     * 로딩 유무 설정
+     *
+     * @param v     뷰
+     * @param value 로딩 유무
+     * @see BaseRatingBar
+     */
+    @BindingAdapter("loading")
+    public static void setRating(@NonNull LoadingLayout v, boolean value) {
+        v.setLoading(value);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        if (loadingView == null && loadingViewId != NO_ID) {
+            loadingView = findViewById(loadingViewId);
+        }
+        if (loadingViewId == NO_ID) {
+            loadingView = inflate(getContext(), R.layout.view_simple_loading, null);
+            addView(loadingView);
+        }
+        if (loadingView != null) {
+            loadingView.bringToFront();
+            loadingView.setVisibility(isLoading ? VISIBLE : GONE);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        if (loadingViewId != NO_ID) {
+            loadingView = null;
+        }
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public boolean dispatchKeyEventPreIme(KeyEvent event) {
+        if (isLoading && event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return super.dispatchKeyEventPreIme(event);
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+        if (loadingView != null) {
+            loadingView.setVisibility(isLoading ? VISIBLE : GONE);
+        }
+    }
+}
