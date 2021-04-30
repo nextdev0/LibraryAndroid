4ed3abb4ba5baeecf46b43bf1417c459c30ff2a0
koipkoi <hrh6914@gmail.com>
2021-04-30 10:35:37 +0900

    FragmentArgumentsBuilder 기능 추가

--- annotationProcessor/src/main/java/com/nextstory/annotationprocessor/util/ClassNames.java
+++ annotationProcessor/src/main/java/com/nextstory/annotationprocessor/util/ClassNames.java
@@ -14,4 +14,13 @@ public final class ClassNames {
     public static final ClassName Context = ClassName.bestGuess("android.content.Context");
     public static final ClassName Intent = ClassName.bestGuess("android.content.Intent");
     public static final ClassName Bundle = ClassName.bestGuess("android.os.Bundle");
+    public static final ClassName Fragment = ClassName.bestGuess("androidx.fragment.app.Fragment");
+    public static final ClassName FragmentManager =
+            ClassName.bestGuess("androidx.fragment.app.FragmentManager");
+    public static final ClassName View = ClassName.bestGuess("android.view.View");
+
+    public static final ClassName LifecycleCallbacks =
+            ClassName.bestGuess("com.nextstory.util.LifecycleCallbacks");
+    public static final ClassName SimpleFragmentLifecycleCallbacks =
+            ClassName.bestGuess("com.nextstory.util.SimpleFragmentLifecycleCallbacks");
 }
--- app/src/main/java/com/nextstory/sample/ui/fragment/Test1Fragment.java
+++ app/src/main/java/com/nextstory/sample/ui/fragment/Test1Fragment.java
@@ -6,9 +6,12 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.nextstory.annotations.FragmentArgument;
+import com.nextstory.annotations.FragmentArgumentsBuilder;
 import com.nextstory.app.BaseFragment;
 import com.nextstory.field.ListLiveData;
 import com.nextstory.field.NonNullLiveData;
+import com.nextstory.sample.R;
 import com.nextstory.sample.databinding.FragmentTestBinding;
 import com.nextstory.sample.ui.dialog.Test2Dialog;
 import com.nextstory.sample.ui.dialog.TestDialog;
@@ -26,15 +29,24 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
  * @author troy
  * @since 1.0
  */
+@FragmentArgumentsBuilder
 public final class Test1Fragment extends BaseFragment<FragmentTestBinding> {
     public final ListLiveData<String> testList = new ListLiveData<>();
     public final NonNullLiveData<String> safeValue = new NonNullLiveData<>("first", true);
     public final NonNullLiveData<String> unsafeValue = new NonNullLiveData<>("first");
     private final OnDestroyDisposables disposables = new OnDestroyDisposables(this);
 
+    @FragmentArgument
+    String arg1 = "test";
+    @FragmentArgument
+    String arg2 = "empty";
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        showToast(arg1);
+        showToast(arg2);
 
         getBinding().setFragment(this);
         getBinding().setLifecycleOwner(this);
@@ -45,6 +57,16 @@ public final class Test1Fragment extends BaseFragment<FragmentTestBinding> {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe(aLong -> testList.add("test_" + random.nextInt(0xffff))));
+    }
+
+    public void argumentsTest() {
+        getParentFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment, new Test1FragmentArgumentsBuilder()
+                        .setArg1("arguments set!")
+                        .build())
+                .addToBackStack(null)
+                .commit();
     }
 
     public void onDialogTestClick() {
