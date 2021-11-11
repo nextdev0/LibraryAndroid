d810e874e6d21a28fa599151bdb81fb490a89087
koipkoi <hrh6914@gmail.com>
2021-11-11 17:12:13 +0900

    Update RxImagePicker.java
    - 보안 문제 수정
    - 불필요한 사진 회전 제거

--- library/src/main/java/com/nextstory/util/RxImagePicker.java
+++ library/src/main/java/com/nextstory/util/RxImagePicker.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.ImageDecoder;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.provider.MediaStore;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.Nullable;
-import androidx.exifinterface.media.ExifInterface;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
@@ -22,9 +20,7 @@ import com.esafirm.imagepicker.features.ReturnMode;
 import com.esafirm.imagepicker.model.Image;
 import com.nextstory.app.AbstractBaseActivity;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -139,20 +135,6 @@ public final class RxImagePicker {
                                         images.get(0).getUri());
                             }
                             Objects.requireNonNull(bitmap);
-                            ExifInterface ei = new ExifInterface(images.get(0).getPath());
-                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                                    ExifInterface.ORIENTATION_UNDEFINED);
-                            switch (orientation) {
-                                case ExifInterface.ORIENTATION_ROTATE_90:
-                                    rotateImage(images.get(0).getUri(), bitmap, 90);
-                                    break;
-                                case ExifInterface.ORIENTATION_ROTATE_180:
-                                    rotateImage(images.get(0).getUri(), bitmap, 180);
-                                    break;
-                                case ExifInterface.ORIENTATION_ROTATE_270:
-                                    rotateImage(images.get(0).getUri(), bitmap, 270);
-                                    break;
-                            }
                             CropImage.activity(images.get(0).getUri()).start(this);
                             return;
                         } catch (IOException e) {
@@ -174,20 +156,6 @@ public final class RxImagePicker {
                                             image.getUri());
                                 }
                                 Objects.requireNonNull(bitmap);
-                                ExifInterface ei = new ExifInterface(image.getPath());
-                                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                                        ExifInterface.ORIENTATION_UNDEFINED);
-                                switch (orientation) {
-                                    case ExifInterface.ORIENTATION_ROTATE_90:
-                                        rotateImage(image.getUri(), bitmap, 90);
-                                        break;
-                                    case ExifInterface.ORIENTATION_ROTATE_180:
-                                        rotateImage(image.getUri(), bitmap, 180);
-                                        break;
-                                    case ExifInterface.ORIENTATION_ROTATE_270:
-                                        rotateImage(image.getUri(), bitmap, 270);
-                                        break;
-                                }
                                 uris.add(image.getUri());
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -223,22 +191,6 @@ public final class RxImagePicker {
             count = getIntent().getIntExtra(EXTRA_COUNT, 1);
             isCropMode = getIntent().getBooleanExtra(EXTRA_CROP_MODE, false);
             start();
-        }
-
-        private void rotateImage(Uri fileUri, Bitmap source, float angle) {
-            Matrix matrix = new Matrix();
-            matrix.postRotate(angle);
-            Bitmap bitmap = Bitmap.createBitmap(source,
-                    0, 0,
-                    source.getWidth(), source.getHeight(),
-                    matrix,
-                    true);
-            try {
-                OutputStream outputStream = getContentResolver().openOutputStream(fileUri);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
         }
 
         private void start() {
